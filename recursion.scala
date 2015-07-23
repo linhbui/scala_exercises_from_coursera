c644d8a9dc5622e0c6a7227f4a562a209bc5ec7f
Linh Bui <mail@linhbui.me>
2015-07-23 16:53:57 -0700

    count change

--- recursion.scala
+++ recursion.scala
@@ -10,12 +10,23 @@ def pascal(c: Int, r: Int): Int =
 
 // check balanced parentheses
 def balance(chars: List[Char]): Boolean = {
-  def balance_rec(remaining: List[Char], open_count: Int): Int =  
-    if (remaining.isEmpty || open_count < 0) open_count
-    else if (remaining.head == '(') balance_rec(remaining.tail, open_count + 1)
-    else if (remaining.head == ')') balance_rec(remaining.tail, open_count - 1) 
-    else balance_rec(remaining.tail, open_count)
-  balance_rec(chars, 0) == 0
+  def balanceRec(chars: List[Char], open_count: Int): Int =  
+    if (chars.isEmpty || open_count < 0) open_count
+    else if (chars.head == '(') balanceRec(chars.tail, open_count + 1)
+    else if (chars.head == ')') balanceRec(chars.tail, open_count - 1) 
+    else balanceRec(chars.tail, open_count)
+
+  balanceRec(chars, 0) == 0
+}
+
+// counts how many different ways you can make change for an amount, given a list of coin denominations
+def countChange(money: Int, coins: List[Int]): Int = {
+  def countChangeRec(money: Int, coins: List[Int]): Int =
+    if (money == 0 || coins.isEmpty) 0
+    else if (money >= coins.head) countChangeRec(money - coins.head, coins) + countChangeRec(coins.head, coins)
+    else countChangeRec(money, coins.tail)
+
+  countChangeRec(money, coins)
 }
 
 def map(lst: List[Int], f: Int=>Int): List[Int] =
